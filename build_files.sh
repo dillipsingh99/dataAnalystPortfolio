54feb7afb1e4b8008e70a31e8295f5e6d2aab842
Dillip Singh <dk381759@gmail.com>
2024-06-04 02:30:51 +0400

    Lets see 03

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
-# echo "BUILD START"
+echo "BUILD START"
+
+source /dapvenv/scripts/activate
+
 # python3.9 -m pip install --root-user-action=ignore
 pip3 install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
-# echo "BUILD END"
+echo "BUILD END"
