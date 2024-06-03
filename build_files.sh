0d5bbc8662c709fd67994e59c467796a0ae0d18a
Dillip Singh <dk381759@gmail.com>
2024-06-04 01:58:58 +0400

    Lets see 02

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # echo "BUILD START"
 # python3.9 -m pip install --root-user-action=ignore
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 # echo "BUILD END"
