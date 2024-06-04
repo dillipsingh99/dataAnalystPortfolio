ac704b5c0cd517f9d859665615ba4057f8e67a17
Dillip Singh <dk381759@gmail.com>
2024-06-04 23:19:17 +0400

    lets do

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip3 install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
